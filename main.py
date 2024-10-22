e6ada2dc36a34187e82956d2ccc053e3d73655a9
derg203 <ddergacev714@gmail.com>
2024-10-22 17:23:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ n = randrange(3,21) #случайное число от 3 до 20
 result = []
 for i in range( 1 , n ) :  #1 число для кратности
     for j in range( 1 , n ) :   #2 число для кратности
-        if i <= j:
+        if i < j:
             if n % ( i + j ) == 0  :
                 result.extend([i,j])
         else:
